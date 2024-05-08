eb97a63c710c7af98fce857aac341585931a7fe8
Noé VINCENT <noe.vincent@protonmail.com>
2024-05-08 16:26:17 +0200

    Debugging

--- main.py
+++ main.py
@@ -22,23 +22,27 @@ class max_heap:
 
 
     def sift_up (self,index):
-        p = self.data[index][0]
-        while self.data[int((index-1)/2)][0] > p:
-            self.switch(index,int((index-1)/2))
+        k = index
+        k_prime = int((k-1)/2)
+        p = self.data[k][0]
+        while self.data[k_prime][0] < p:
+            self.switch(k,k_prime)
+            k = k_prime
+            k_prime = int((k-1)/2)
 
     def sift_down(self,index):
         k = index
         k_prime = k
-        if 2*k+1 < self.size and self.data[2*k+1][0] < self.data[k_prime][0]:
+        if 2*k+1 < self.size and self.data[2*k+1][0] > self.data[k_prime][0]:
             k_prime = 2*k+1 
-        if 2*k+2 < self.size and self.data[2*k+2][0] < self.data[k_prime][0]:
+        if 2*k+2 < self.size and self.data[2*k+2][0] > self.data[k_prime][0]:
             k_prime = 2*k+2
         while k!=k_prime:
             self.switch(k,k_prime)
             k = k_prime
-            if 2*k+1 < self.size and self.data[2*k+1][0] < self.data[k_prime][0]:
+            if 2*k+1 < self.size and self.data[2*k+1][0] > self.data[k_prime][0]:
                 k_prime = 2*k+1 
-            if 2*k+2 < self.size and self.data[2*k+2][0] < self.data[k_prime][0]:
+            if 2*k+2 < self.size and self.data[2*k+2][0] > self.data[k_prime][0]:
                 k_prime = 2*k+2
 
 
@@ -47,8 +51,9 @@ class max_heap:
         self.size += 1
         self.sift_up(self.size-1)
     
-    def pop (self):        
-        self.switch(0, self.size-1)
+    def pop (self):
+        if self.size > 1:        
+            self.switch(0, self.size-1)
         res = self.data.pop()
         self.size-=1
         self.sift_down(0)
@@ -65,14 +70,16 @@ class graph:
         self.graph = {}
     
     def add_edge(self,x,y,w):
+        if x not in self.graph :
+            self.graph[x] = []
         self.graph[x].append((y,w))
 
     def edges(self,x):
         return self.graph[x]
 
 
-DATA = [('s',2,4),('s',2,3),('s',3,4),('s',4,4),('s',5,3),('s',6,3),('s',7,7),('s',8,3),('s',9,2),('s',10,2),(0,1000,3),(1,11,5),(1,45,5),(2,45,5),(45,44,5),(44,43,5),(43,41,5),(2,3,4),(3,4,4),(4,5,4),(5,42,4),(41,42,4),(42,6,4),(41,40,5),(40,46,2),(46,38,6),(38,37,6),(6,37, 5),(6,36,5),(37,36,5),(7,36,7),(36,39,12),(38,39,3),(39,1001,12),(39,20,4),(36,35,3),(35,34,3),(19,34,3),(8,19,6),(19,23,1),(24,23,4),(18,24,3),(23,25,3),(23,27,4),(27,29,4),(29,21,4),(20,21,11),(21,54,3),(54,55,3),(25,31,4),(31,32,4),(25,26,5),(26,28,5),(29,28,4),(28,30,4),(30,33,3),(28,22,4),(21,22,7),(22,33,7),(33,1002,8),(51,33,7),(53,51,2),(52,53,2),(52,49,5),(55,52,2),(54,49,3),(49,51,3),(22,49,5),(10,12,5),(10,9,5),(9,16,4),(12,15,4),(12,11,5),(15,17,3), (16,17,5),(16,18,3),(17,18,4) ]
-
+DATA = [('s',0,4),('s',2,4),('s',2,3),('s',3,4),('s',4,4),('s',5,3),('s',6,3),('s',7,7),('s',8,3),('s',9,2),('s',10,2),(0,1000,3),(1,11,5),(1,45,5),(2,45,5),(45,44,5),(44,43,5),(43,41,5),(2,3,4),(3,4,4),(4,5,4),(5,42,4),(41,42,4),(42,6,4),(41,40,5),(40,46,2),(46,38,6),(38,37,6),(6,37, 5),(6,36,5),(37,36,5),(7,36,7),(36,39,12),(38,39,3),(39,1001,12),(39,20,4),(36,35,3),(35,34,3),(19,34,3),(8,19,6),(19,23,1),(24,23,4),(18,24,3),(23,25,3),(23,27,4),(27,29,4),(29,21,4),(20,21,11),(21,54,3),(54,55,3),(25,31,4),(31,32,4),(25,26,5),(26,28,5),(29,28,4),(28,30,4),(30,33,3),(28,22,4),(21,22,7),(22,33,7),(33,1002,8),(51,33,7),(53,51,2),(52,53,2),(52,49,5),(55,52,2),(54,49,3),(49,51,3),(22,49,5),(10,12,5),(10,9,5),(9,16,4),(12,15,4),(12,11,5),(15,17,3), (16,17,5),(16,18,3),(17,18,4) ]
+N = len(DATA)
 
 def rebuild_path(g,partial, start,end):
     path = []
@@ -88,7 +95,9 @@ def rebuild_path(g,partial, start,end):
                 candidates.append((min(w,partial[y]),y))
         _,next=max(candidates)
         current = next
-    return path.reverse()
+    path.append(start)
+    path.reverse()
+    return path
 
 def dijkstra (g, start, end):
     """
@@ -101,9 +110,9 @@ def dijkstra (g, start, end):
     """
     node =  start
     bag = max_heap()
-    bag.push(0,node)
+    bag.push(float("inf"),node)
     partial = {}
-    partial[node] = 0
+    partial[node] = float("inf")
     treated = set()
     while end not in treated: 
         (capa,node) = bag.pop()
@@ -119,9 +128,12 @@ def dijkstra (g, start, end):
     return widest_path
 
 def main():
-    g = graph
-    for i in range(len(DATA)):
+    g = graph(N)
+    for i in range(N):
         x,y,w = DATA[i]
         g.add_edge(x,y,w)
         g.add_edge(y,x,w)
+    p = dijkstra(g,'s',1000)
+    print(p)
     
+main()
